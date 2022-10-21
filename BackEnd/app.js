b8502c2b094c292902dc2c3a3f8058fc0ff3423e
facu <facu11ja@gmail.com>
2022-10-21 05:20:27 -0300

    Se crea servidor y se agregan algunas  rutas

---
+++ BackEnd/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const path = require('node:path');
+const routes_index = require('./routes/index');
+const routes_users = require('./routes/users');
+const routes_account = require('./routes/account');
+
+//setings
+app.use(express.text());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.set("view engine", "ejs");
+app.set('views', path.join(__dirname,'/views'));
+app.use(express.static('public'));
+
+
+//Routes
+app.use(routes_index);
+app.use(routes_users)
+app.use(routes_account)
+app.listen(3000);
+console.log("server on http://localhost:3000/");
---
+++ BackEnd/routes/users.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/miperfil", (req, res) => {
+  res.render("mi_perfil");
+});
+
+router.get("/editarperfil",(req,res)=>{
+  res.render("editar_perfil");
+})
+
+router.get("/mispublicaciones",(req,res)=>{
+  res.render("misPublicaciones");
+})
+module.exports = router;
